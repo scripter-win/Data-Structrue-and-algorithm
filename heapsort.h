70d7f0b9ee8be01e98bc13c75497d560448cad88
scripter <t279507736@outlook.com>
2017-04-19 11:48:37 +0800

    Add files via upload

---
+++ heapsort.h
@@ -0,0 +1,22 @@
+#include<Vector.h>
+
+int LeftChild(int i)
+{
+    return i*2;
+}
+
+int RightChild(int i)
+{
+    return i*2+1;
+}
+
+int Parent(int i)
+{
+    return i/2;
+}
+
+void MaxHeapify(Vector v,int i)
+{
+    
+}
+
